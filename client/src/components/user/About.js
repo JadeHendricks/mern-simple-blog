9c3ae97fbb56c585cfe8ffb2cdc0e8b8f81c5112
jade <hendricksjade@gmail.com>
2020-06-15 01:17:27 +0200

    updating the about page

--- client/src/components/user/About.js
+++ client/src/components/user/About.js
@@ -3,27 +3,48 @@ import React from 'react';
 const About = () => {
   return (
     <section>
-      <div class="container">
-        <div class="col-md-8 offset-md-2">
-          <h1 class="mt-5">Name of Person</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-            Odit iste consequatur porro laborum provident qui adipisci doloremque! Aut aspernatur unde accusantium numquam.
-            laudantium amet ut laborum qui, distinctio vero esse reprehenderit culpa! Expedita modi doloremque rem pariatur 
-            recusandae error quibusdam omnis nemo ipsa, obcaecati nostrum fuga veniam sed! Praesentium distinctio neque fugit 
-            ullam quisquam quis repudiandae eaque recusandae maiores itaque soluta et optio, quibusdam ex fugiat dicta eveniet 
-            in minus explicabo assumenda, voluptatum culpa non. Debitis perferendis dicta id quod commodi, tenetur nam voluptas, 
-            error facere dolore recusandae. Minima reiciendis sapiente accusamus! sint fugit explicabo eaque aliquam repudiandae fuga excepturi, 
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-            Odit iste consequatur porro laborum provident qui adipisci doloremque! Aut aspernatur unde accusantium numquam.
-            laudantium amet ut laborum qui, distinctio vero esse reprehenderit culpa! Expedita modi doloremque rem pariatur 
-            recusandae error quibusdam omnis nemo ipsa, obcaecati nostrum fuga veniam sed! Praesentium distinctio neque fugit 
-            ullam quisquam quis repudiandae eaque recusandae maiores itaque soluta et optio, quibusdam ex fugiat dicta eveniet 
-            in minus explicabo assumenda, voluptatum culpa non. Debitis perferendis dicta id quod commodi, tenetur nam voluptas, 
-            error facere dolore recusandae. Minima reiciendis sapiente accusamus! sint fugit explicabo eaque aliquam repudiandae fuga excepturi, 
-          </p>
+      <div className="container">
+        <div className="col-md-8 offset-md-2">
+          <h1 className="mt-5 mb-3">Morgan Freeman</h1>
+            <hr/>
+            <h5 className="h5 text-success">Who Is Morgan Freeman?</h5>
+            <p>
+              Morgan Freeman was born June 1, 1937, in Memphis, Tennessee. 
+              Although he loved acting, Freeman joined the Air Force after high school to become a fighter pilot. 
+              He later realized it wasn't what he'd wanted and thus, began his acting career.
+              After years of small parts and limited success, he began to land big roles and win critical and popular acclaim. 
+              He's now one of Hollywood's most respected stars.
+            </p>
+            <h5 className="h5 text-success">Early Passion for Acting</h5>
+            <p>
+              Actor, director and narrator Morgan Freeman was born on June 1, 1937, in Memphis, Tennessee. 
+              The youngest of five children born to barber Morgan Porterfield Freeman, Sr. 
+              and schoolteacher Mayme Edna, Freeman was raised in Chicago and Mississippi in a low-income home. 
+              Not long after he was born, Morgan's parents, like so many other African-Americans struggling under the pressures 
+              of the Jim Crow South, relocated to Chicago to find work. While his parents looked for jobs, 
+              Freeman remained with his maternal grandmother in Charleston, Mississippi.
+            </p>
+            <p>
+              At the age of six, Freeman's grandmother died and he moved north to be with his mother, 
+              who had already separated from her alcoholic husband. They later moved to Tennessee and eventually back to Mississippi, 
+              where Mayme Edna settled her family in Greenwood.
+            </p>
+            <p>
+              As a kid, Freeman spent a good portion of his time scraping together enough money to see movies, 
+              where he formed an early admiration for actors like Gary Cooper, Spencer Tracy and Sidney Poitier. 
+              It was by chance that Freeman himself got into acting. He was in junior high school and, 
+              as punishment for pulling out a chair from underneath a girl he had a crush on, 
+              Freeman was ordered to participate in the school's drama competition. 
+              To his surprise, and probably school administrators, the 12-year-old proved to be an immediate natural on the stage, 
+              taking top honors in the program.
+            </p>
+            <p>
+              But while Freeman loved to act, flying—in particular the idea of being a fighter pilot—was in his heart of hearts. 
+              And so, upon graduating high school in 1955, Morgan turned down a partial drama scholarship and joined the U.S. Air Force. 
+              The military, though, proved to be much different than what he'd expected. Instead of darting around the skies, 
+              Freeman was relegated to on-the-ground activity as a mechanic and radar technician. 
+              He also realized that he didn't want to be shooting down other people.
+            </p>
         </div>
       </div>
     </section>
